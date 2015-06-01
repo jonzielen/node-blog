b1f7d8026c004c44ee81738675cf95aad65d3e13
Jon Zielenkievicz <jonzielen@gmail.com>
2015-06-01 19:40:20 -0400

    add Blog setup

--- nodeBlog/routes/index.js
+++ nodeBlog/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var siteTitle = 'Welcome to my FIRST Node/Express/Mongo blog!';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('homepage', {
+    homepage: true,
+    title: siteTitle
+  });
+});
+
+/* GET Blog Page */
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'Blog | '+siteTitle });
 });
 
 module.exports = router;
